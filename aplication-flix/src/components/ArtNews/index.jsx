97195417a5871f86fba8b60fb943f1dc01bf9f93
Danilo Victor <danilo-almeida12@hotmail.com>
2022-01-13 22:14:40 -0300

    linksarticle

--- aplication-flix/src/components/ArtNews/index.jsx
+++ aplication-flix/src/components/ArtNews/index.jsx
@@ -11,11 +11,11 @@ const ArtNews = () => {
                 <div className="artnews">
 
                     <a target="_blank" href="https://www.cnnbrasil.com.br/entretenimento/confira-os-principais-filmes-que-chegam-aos-cinemas-em-2022/">Filmes que chegaram ao cinema em 2022 - Clique para conferir</a> <br/>
-                    <a href="">Link2</a> <br/>
-                    <a href="">Link3</a> <br/>
-                    <a href="">Link4</a> <br/>
-                    <a href="">Link5</a> <br/>
-                    <a href="">Link6</a> <br/>
+                    <a target="_blank" href="https://g1.globo.com/pop-arte/tv-e-series/noticia/2022/01/11/series-de-2022-quais-as-estreias-do-ano-e-quem-volta-em-novas-temporadas-veja-destaques.ghtml">Séries que estão previstas para 2022 - Clique para conferir</a> <br/>
+                    <a target="_blank" href="https://www.uol.com.br/splash/colunas/roberto-sadovski/2022/01/07/os-10-melhores-filmes-de-2021.htm">Melhores filmes de 2021 - Clique aqui para conferir</a> <br/>
+                    <a target="_blank" href="https://www.eldogomes.com.br/7-canais-no-youtube-que-cobrem-o-universo-do-cinema/">Canais do Youtube sobre filmes, desenhos e séries - Clique aqui para conferir</a> <br/>
+                    <a target="_blank" href="https://www.dci.com.br/dci-mais/cinema-e-tv/filmes-de-desenho-da-netflix-2021/130461/">Filmes em desenho para assistir em familía - Clique aqui para conferir</a> <br/>
+                    <a target="_blank" href="https://g1.globo.com/pop-arte/cinema/">Noticias sobre cinema para conferir e ficar atualizado - Clique aqui para conferir</a> <br/>
                     
 
                 </div>
