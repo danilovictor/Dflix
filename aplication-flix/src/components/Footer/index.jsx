4bde18e71c97141465b620955959ef7e8badb27d
Danilo Victor <danilo-almeida12@hotmail.com>
2022-01-19 21:56:35 -0300

    cssfooter

--- aplication-flix/src/components/Footer/index.jsx
+++ aplication-flix/src/components/Footer/index.jsx
@@ -4,7 +4,10 @@ import './style.css'
 const Footer = ()=>{
     return( 
         <div>
+            <footer>
             <h2 className="footer-title">Desenvolvido por Danilo Victor - 2022</h2>
+            </footer>
+            
         </div>
     )
 }
